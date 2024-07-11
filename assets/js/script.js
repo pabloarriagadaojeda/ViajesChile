6684a6fadbf42d698cc4204c71ede35fbfc2cd7c
Pablo Arriagada Ojeda <pabloarriagadarq@gmail.com>
2024-07-10 23:20:13 -0400

    Código documentado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
+
+    /* Alerta al enviar el mensaje */
     $("#enviarCorreo").click(function () {
         alert("Mensaje enviado correctamente, te estaremos contactando pronto. ¡Buen día!");
     });
     
-    
+    /* Mostrar los textos en la sección de presentación */
     $(".icono").on("click", function () {
         $(this).siblings(".texto-presentacion").slideToggle();
     });
 
+    /* Despliega los tooltip */
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
