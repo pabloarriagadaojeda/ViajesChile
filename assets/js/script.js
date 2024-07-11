d40c070fb37194b342e8226f022ed54a2733ad99
Pablo Arriagada Ojeda <pabloarriagadarq@gmail.com>
2024-07-10 23:12:39 -0400

    Mejoras en UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#enviarCorreo").click(function () {
-        alert("El correo fue enviado correctamente...");
+        alert("Mensaje enviado correctamente, te estaremos contactando pronto. ¡Buen día!");
     });
     
     
